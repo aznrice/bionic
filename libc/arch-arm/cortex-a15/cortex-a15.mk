5d647f60945b54be096709cbd5f7c3e76d69c5dc
synergy <synergye@codefi.re>
2013-06-25 17:43:09 -0700

    bionic: add linaro-a9 cortex-strings rev 65 memchr routine cortex-a15 will use the a9 memchr as well due to still performing better than the c memchr. Generic uses the c AOSP memchr

--- libc/arch-arm/cortex-a15/cortex-a15.mk
+++ libc/arch-arm/cortex-a15/cortex-a15.mk
@@ -1,3 +1,4 @@
+$(call libc-add-cpu-variant-src,MEMCHR,arch-arm/cortex-a9/bionic/memchr.S)
 $(call libc-add-cpu-variant-src,MEMCPY,arch-arm/cortex-a15/bionic/memcpy.S)
 $(call libc-add-cpu-variant-src,MEMSET,arch-arm/cortex-a15/bionic/memset.S)
 $(call libc-add-cpu-variant-src,STRCHR,arch-arm/cortex-a9/bionic/strchr.S)
