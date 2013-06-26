44b04a8c22d8487102814c4e86cfdcdd2d6fdc1f
synergy <synergye@codefi.re>
2013-06-25 17:38:51 -0700

    bionic: use optimized strchr for krait as well

--- libc/arch-arm/krait/krait.mk
+++ libc/arch-arm/krait/krait.mk
@@ -1,5 +1,6 @@
 $(call libc-add-cpu-variant-src,MEMCPY,arch-arm/krait/bionic/memcpy.S)
 $(call libc-add-cpu-variant-src,MEMSET,arch-arm/krait/bionic/memset.S)
+$(call libc-add-cpu-variant-src,STRCHR,arch-arm/cortex-a9/bionic/strchr.S)
 $(call libc-add-cpu-variant-src,STRCMP,arch-arm/krait/bionic/strcmp.S)
 
 include bionic/libc/arch-arm/generic/generic.mk
