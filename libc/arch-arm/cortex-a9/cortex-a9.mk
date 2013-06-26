a9ba4605f6b559a8c96dab755ef1620475aefc8d
synergy <synergye@codefi.re>
2013-06-25 17:30:00 -0700

    bionic: add linaro-a9 cortex-strings rev 54 strchr routine cortex-a15 will use the a9 strchr as well due to still performing better than the c++ strchr. Generic uses the c++ AOSP strchr

--- libc/arch-arm/cortex-a9/cortex-a9.mk
+++ libc/arch-arm/cortex-a9/cortex-a9.mk
@@ -1,5 +1,6 @@
 $(call libc-add-cpu-variant-src,MEMCPY,arch-arm/cortex-a9/bionic/memcpy.S)
 $(call libc-add-cpu-variant-src,MEMSET,arch-arm/cortex-a9/bionic/memset.S)
+$(call libc-add-cpu-variant-src,STRCHR,arch-arm/cortex-a9/bionic/strchr.S)
 $(call libc-add-cpu-variant-src,STRCMP,arch-arm/cortex-a9/bionic/strcmp.S)
 
 include bionic/libc/arch-arm/generic/generic.mk
